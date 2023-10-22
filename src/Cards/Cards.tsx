0143414afcba94701a92fe30bc3b4c76c79b0134
Sergeev Mikhail <sergeev-michael@mail.ru>
2023-10-22 15:59:46 +0600

    Create 'Lib' directory on 'src' folder. Then create two .ts files 'Card' and 'CardDeck'

---
+++ src/Cards/Cards.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+interface Props {
+    rank: string;
+    suit: string;
+}
+
+const Cards: React.FC<Props> = ({ rank, suit }) => {
+    let symbol: string;
+    switch (suit) {
+        case 'diams':
+            symbol = '♦';
+            break;
+        case 'hearts':
+            symbol = '♥';
+            break;
+        case 'clubs':
+            symbol = '♣';
+            break;
+        case 'spades':
+            symbol = '♠';
+            break;
+        default:
+            symbol = '';
+    }
+
+    return (
+    <div className="playingCards faceImages">
+        <span className={`card rank-${rank} ${suit}`}>
+            <span className="rank">{rank}</span>
+            <span className="suit">{symbol}</span>
+        </span>
+     </div>
+    );
+};
+
+export default Cards;
